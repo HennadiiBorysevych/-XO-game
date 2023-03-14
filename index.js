a8c1dfcd68871bbf270e2bd5585e5d79fc4e54a7
Hennadii Borysevych <genuch3@gmail.com>
2023-03-14 13:35:59 +0200

    final

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ let playerTurn = "x";
 let moves = 0;
 let isGameOver = false;
 const span = document.getElementsByTagName("span");
+let restartButton = `<button onclick="resetGame()">Restart Game</button>`;
 
-function play(y) {
-  if (y.dataset.player == "none" && window.isGameOver == false) {
-    y.innerHTML = playerTurn;
-    y.dataset.player = playerTurn;
+function play(box) {
+  if (box.dataset.player == "none" && isGameOver === false) {
+    box.innerHTML = playerTurn;
+    box.dataset.player = playerTurn;
     moves++;
-    if (playerTurn == "x") {
-      playerTurn = "o";
-    } else if (playerTurn == "o") {
-      playerTurn = "x";
-    }
+    playerTurn = playerTurn == "x" ? "o" : "x";
   }
 
   checkWinner(1, 2, 3);
@@ -23,7 +20,8 @@ function play(y) {
   checkWinner(3, 6, 9);
   checkWinner(1, 5, 9);
   checkWinner(3, 5, 7);
-  if (moves == 9 && isGameOver == false) {
+
+  if (moves == 9 && isGameOver === false) {
     draw();
   }
 }
@@ -32,16 +30,16 @@ function checkWinner(a, b, c) {
   b--;
   c--;
   if (
-    (span[a].dataset.player === span[b].dataset.player &&
-      span[b].dataset.player === span[c].dataset.player &&
-      span[a].dataset.player === span[c].dataset.player &&
-      span[a].dataset.player === "x") ||
-    (span[a].dataset.player === "o" && isGameOver == false)
+    isGameOver == false &&
+    span[a].dataset.player === span[b].dataset.player &&
+    span[b].dataset.player === span[c].dataset.player &&
+    span[a].dataset.player === span[c].dataset.player &&
+    (span[a].dataset.player === "x" || span[a].dataset.player === "o")
   ) {
-    span[a].parentNode.className += "activeBox";
-    span[b].parentNode.className += "activeBox";
-    span[c].parentNode.className += "activeBox";
-    isGameOver(a);
+    span[a].parentNode.className += " activeBox";
+    span[b].parentNode.className += " activeBox";
+    span[c].parentNode.className += " activeBox";
+    gameOver(a);
   }
 }
 function playAgain() {
@@ -49,6 +47,43 @@ function playAgain() {
     .getElementsByClassName("alert")[0]
     .parentNode.removeChild(document.getElementsByClassName("alert")[0]);
   resetGame();
-  window.isGameOver = false;
-  for (let i = 0; i < span.length; i++) {}
+  isGameOver = false;
+  for (let i = 0; i < span.length; i++) {
+    span[i].parentNode.className = span[i].parentNode.cloneName.replace(
+      "activeBox",
+      ""
+    );
+  }
+}
+
+function resetGame() {
+  for (let i = 0; i < span.length; i++) {
+    span[i].dataset.player = "none";
+    span[i].innerHTML = "&nbsp;";
+  }
+  playerTurn = "x";
+  location.reload();
+}
+function gameOver(a) {
+  let gameOverAlert =
+    "<b>GAME OVER</b><br><br> Player " +
+    span[a].dataset.player.toUpperCase() +
+    " Win! <br><br>" +
+    restartButton;
+  let div = document.createElement("div");
+  div.className = "alert";
+  div.innerHTML = gameOverAlert;
+  document.getElementsByTagName("body")[0].appendChild(div);
+  isGameOver = true;
+  moves = 0;
+}
+
+function draw() {
+  let drawAlert = "<b>Draw!</b><br><br>" + restartButton;
+  let div = document.createElement("div");
+  div.className = "alert";
+  div.innerHTML = drawAlert;
+  document.getElementsByTagName("body")[0].appendChild(div);
+  isGameOver = true;
+  moves = 0;
 }
